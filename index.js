7f0ab8d7d7381d42e295647b6afaadbe06b1cf51
nishatlea <nishataurora1956@gmail.com>
2022-10-04 12:39:41 +0600

    Post Method

--- index.js
+++ index.js
@@ -10,11 +10,26 @@
 
 //2nd example
 
+// var express = require("express");
+// var app = express();
+
+// app.get("/hello", function (req, res) {
+//   res.send("Hello World hello!");
+// });
+
+// app.listen(3001);
+
+//Example 3 : Using Multiple methods
 var express = require("express");
 var app = express();
 
-app.get("/hello", function (req, res) {
-  res.send("Hello World hello!");
+// app.get("/hello", function (req, res) {
+//   res.send("Hello World!");
+// });
+//For posting purpose I have to write in the terminal this command:
+//curl -X POST "http://localhost:3000/hello"
+app.post("/hello", function (req, res) {
+  res.send("You just called the post method at '/hello'!\n");
 });
 
 app.listen(3001);
