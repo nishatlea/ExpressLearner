e091516459773295717edbf23e81d68a71846466
nishatlea <nishataurora1956@gmail.com>
2022-10-04 12:34:48 +0600

    Example 2

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+//1st example
+
+// var express = require("express");
+// var app = express();
+
+// app.get("/", function (req, res) {
+//   res.send("Hello World");
+// });
+// app.listen(3001);
+
+//2nd example
+
 var express = require("express");
 var app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.get("/hello", function (req, res) {
+  res.send("Hello World hello!");
 });
+
 app.listen(3001);
