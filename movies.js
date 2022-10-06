60716fc0382cce6f6114df108aab6adf93396619
nishatlea <nishataurora1956@gmail.com>
2022-10-06 13:17:33 +0600

    post,put,delete examples are given but showing error due to regex

--- movies.js
+++ movies.js
@@ -15,38 +15,101 @@ module.exports = router;
 // });
 
 //route to get a specific movie by its id.
-router.get("/:id([0-9]{3,})", function (req, res) {
-  var currMovie = movies.filter(function (movie) {
-    if (movie.id == req.params.id) {
-      return true;
-    }
-  });
-  if (currMovie.length == 1) {
-    res.json(currMovie[0]);
-  } else {
-    res.status(404); //Set status to 404 as movie was not found
-    res.json({ message: "Not Found" });
-  }
-});
+// router.get("/:id([0-9]{3,})", function (req, res) {
+//   var currMovie = movies.filter(function (movie) {
+//     if (movie.id == req.params.id) {
+//       return true;
+//     }
+//   });
+//   if (currMovie.length == 1) {
+//     res.json(currMovie[0]);
+//   } else {
+//     res.status(404); //Set status to 404 as movie was not found
+//     res.json({ message: "Not Found" });
+//   }
+// });
 
 //post route
-router.post("/", function (req, res) {
-  //Check if all fields are provided and are valid:
-  if (
-    !req.body.name.toString().match(/^[.]$/g) ||
-    !req.body.year.toString().match(/^[0-9]{4}$/g) ||
-    !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g)
-  ) {
-    res.status(400);
-    res.json({ message: "Bad Request" });
-  } else {
-    var newId = movies[movies.length - 1].id + 1;
-    movies.push({
-      id: newId,
-      name: req.body.name,
-      year: req.body.year,
-      rating: req.body.rating,
-    });
-    res.json({ message: "New movie created.", location: "/movies/" + newId });
-  }
-});
+// router.post("/", function (req, res) {
+//   //Check if all fields are provided and are valid:
+//   if (
+//     !req.body.name.toString().match(/^[.]$/g) ||
+//     !req.body.year.toString().match(/^[0-9]{4}$/g) ||
+//     !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g)
+//   ) {
+//     res.status(400);
+//     res.json({ message: "Bad Request" });
+//   } else {
+//     var newId = movies[movies.length - 1].id + 1;
+//     movies.push({
+//       id: newId,
+//       name: req.body.name,
+//       year: req.body.year,
+//       rating: req.body.rating,
+//     });
+//     res.json({ message: "New movie created.", location: "/movies/" + newId });
+//   }
+// });
+
+//put method
+// router.put("/:id", function (req, res) {
+//   //Check if all fields are provided and are valid:
+//   if (
+//     !req.body.name ||
+//     !req.body.year.toString().match(/^[0-9]{4}$/g) ||
+//     !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g) ||
+//     !req.params.id.toString().match(/^[0-9]{3,}$/g)
+//   ) {
+//     res.status(400);
+//     res.json({ message: "Bad Request" });
+//   } else {
+//     //Gets us the index of movie with given id.
+//     var updateIndex = movies
+//       .map(function (movie) {
+//         return movie.id;
+//       })
+//       .indexOf(parseInt(req.params.id));
+
+//     if (updateIndex === -1) {
+//       //Movie not found, create new
+//       movies.push({
+//         id: req.params.id,
+//         name: req.body.name,
+//         year: req.body.year,
+//         rating: req.body.rating,
+//       });
+//       res.json({
+//         message: "New movie created.",
+//         location: "/movies/" + req.params.id,
+//       });
+//     } else {
+//       //Update existing movie
+//       movies[updateIndex] = {
+//         id: req.params.id,
+//         name: req.body.name,
+//         year: req.body.year,
+//         rating: req.body.rating,
+//       };
+//       res.json({
+//         message: "Movie id " + req.params.id + " updated.",
+//         location: "/movies/" + req.params.id,
+//       });
+//     }
+//   }
+// });
+
+//delete route
+// router.delete("/:id", function (req, res) {
+//   var removeIndex = movies
+//     .map(function (movie) {
+//       return movie.id;
+//     })
+//     .indexOf(req.params.id); //Gets us the index of movie with given id.
+
+//   if (removeIndex === -1) {
+//     res.json({ message: "Not found" });
+//   } else {
+//     movies.splice(removeIndex, 1);
+//     res.send({ message: "Movie id " + req.params.id + " removed." });
+//   }
+// });
