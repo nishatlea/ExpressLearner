491d21cad7f86fac95a5bb836650be892a1553d0
nishatlea <nishataurora1956@gmail.com>
2022-10-07 11:44:45 +0600

    Get data using sequelize

--- test.js
+++ test.js
@@ -7,12 +7,22 @@
 //connecting with xampp mysql
 //sudo /opt/lampp/lampp start
 //sudo /opt/lampp/lampp stop
+//sudo /etc/init.d/mysql stop
+//sudo  /opt/lampp/lampp restart
 
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("edoverse-nft-db", "root", "", {
+const dbPoolConfig = {
+  max: 5,
+  min: 0,
+  acquire: 30000,
+  idle: 10000,
+};
+const sequelize = new Sequelize("DemoDatabase", "root", "", {
   host: "localhost",
   dialect:
     "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+  operatorsAliases: false,
+  pool: dbPoolConfig,
 });
 
 sequelize
@@ -23,3 +33,39 @@ sequelize
   .catch((err) => {
     console.error("Unable to connect to the database:", err);
   });
+
+const User = sequelize.define(
+  "User",
+  {
+    // Model attributes are defined here
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      // allowNull defaults to true
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
+
+// Create a new user
+//const jane = User.create({ firstName: "Jane", lastName: "Doe" });
+//console.log("Jane's auto-generated ID:", jane.id);
+async function DataVal() {
+  // Find all users
+  users = await User.findAll();
+  //console.log(users); // true
+  console.log("Read Table data: ");
+  //await console.log(users.firstName);
+  console.log(users.every((user) => user instanceof User)); // true
+  console.log("All users:", JSON.stringify(users, null, 2));
+}
+
+DataVal();
